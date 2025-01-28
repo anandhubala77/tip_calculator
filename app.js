36b046b434891d376a3d22404734cac4b18930ad
anandhu balakrishnan <you@example.comanandhubalakrishnan77@gmail.com>
2025-01-28 22:14:16 +0530

    icon change

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 function calculateTip() {
-    const amount = parseInt(document.querySelector("#billAmount").value);
-    const tip = document.querySelector("#tipPercentage").value;
-  
-    if (isNaN(amount) || tip === "") {
-      alert("Please fill the fields");
-    } else {
-      const tipAmount = (amount * tip) / 100;
-      const totalBill = amount + tipAmount;
-      document.querySelector("#tipAmount").innerHTML = `${tipAmount.toFixed(2)}`;
-      document.querySelector("#totalBill").innerHTML = `${totalBill.toFixed(2)}`;
-    }
+  const amount = parseInt(document.querySelector("#billAmount").value);
+  const tip = document.querySelector("#tipPercentage").value;
+
+  if (isNaN(amount) || tip === "") {
+    alert("Please fill the fields");
+  } else {
+    const tipAmount = (amount * tip) / 100;
+    const totalBill = amount + tipAmount;
+    document.querySelector("#tipAmount").innerHTML = `₹ ${tipAmount.toFixed(2)}`;
+    document.querySelector("#totalBill").innerHTML = `₹ ${totalBill.toFixed(2)}`;
   }
-  
-  function reset() {
-    document.querySelector("#billAmount").value = "";
-    document.querySelector("#tipPercentage").value = "";
-    document.querySelector("#tipAmount").innerHTML = "<i class=\"fa-solid fa-rupee-sign\"></i> 0.00";
-    document.querySelector("#totalBill").innerHTML = "<i class=\"fa-solid fa-rupee-sign\"></i> 0.00";
-  }
-  
+}
+
+function reset() {
+  document.querySelector("#billAmount").value = "";
+  document.querySelector("#tipPercentage").value = "";
+  document.querySelector("#tipAmount").innerHTML = "₹ 0.00";
+  document.querySelector("#totalBill").innerHTML = "₹ 0.00";
+}
