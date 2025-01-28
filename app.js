ed2120f211d0e05c075d0b53e62b65d5affa33df
anandhu balakrishnan <you@example.comanandhubalakrishnan77@gmail.com>
2025-01-28 22:19:42 +0530

    error chmage3

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ function calculateTip() {
   } else {
     const tipAmount = (amount * tip) / 100;
     const totalBill = amount + tipAmount;
+   
     document.querySelector("#tipAmount").innerHTML = `₹ ${tipAmount.toFixed(2)}`;
     document.querySelector("#totalBill").innerHTML = `₹ ${totalBill.toFixed(2)}`;
   }
@@ -15,6 +16,7 @@ function calculateTip() {
 function reset() {
   document.querySelector("#billAmount").value = "";
   document.querySelector("#tipPercentage").value = "";
+
   document.querySelector("#tipAmount").innerHTML = "₹ 0.00";
   document.querySelector("#totalBill").innerHTML = "₹ 0.00";
 }
